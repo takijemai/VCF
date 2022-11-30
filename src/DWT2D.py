949331c20d161985afd7f88af44ae6d3bafcfe82
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2022-11-30 22:51:11 +0100

    .

--- src/DWT2D.py
+++ src/DWT2D.py
@@ -37,38 +37,83 @@ class DWT2D(YCoCg.YCoCg):
         img = self.read()
         img_128 = img.astype(np.int16) - 128
         YCoCg_img = from_RGB(img_128)
-        decom_img = analyze(YCoCg_img, self.wavelet, self.args.levels)
+        decom_img = analyze(YCoCg_img, self.wavelet, self.levels)
         decom_k = self.quantize_decom(decom_img)
-        self.required_bytes += self.save_decom(k_decom)
+        self.save_decom(decom_k)
         rate = (self.required_bytes*8)/(img.shape[0]*img.shape[1])
         return rate
 
-    def quantize_decom(self, decom):
-        LL = decom[0]
-        LL_k = self.quantize(LL)
-        decom_k = [add_offset(LL_k)]
-        sbc_counter = 1
-        for sr in decom[1:]:
-            sr_k = []
-            for sb in sr: # sb = subband
-                Q = Quantizer(Q_step=sb_Q_factor)
-            sb_k = Q.quantize(sb)
-            sb_dQ = Q.dequantize(sb_k)
-            sr_k.append(add_offset(sb_k))
-            sr_dQ.append(sb_dQ)
-            sbc_counter += 1
-        decom_k.append(tuple(sr_k))
-        decom_dQ.append(tuple(sr_dQ))
-
     def decode(self):
-        k = self.read()
-        DWT_y = self.dequantize(k)
-        YCoCg_y = synthesize(DWT_y, self.wavelet, self.args.levels)
+        decom_k = self.read_decom()
+        decom_y = self.dequantize_decom(k)
+        YCoCg_y = synthesize(decom_y, self.wavelet, self.levels)
         y_128 = to_RGB(YCoCg_y)
         y = (y_128.astype(np.int16) + 128)
         y = np.clip(y, 0, 255).astype(np.uint8)
-        rate = self.save(y)
+        self.save(y)
+        rate = (self.required_bytes*8)/(k.shape[0]*k.shape[1])
         return rate
+
+    def quantize_decom(self, d):
+        k = [self.quantize(d[0])] # LL subband
+        sb_counter = 1 # sb = subband
+        for sr_d in d[1:]: # sr = spatial resolution
+            sr_k = []
+            for sb_d in sr_d:
+                sb_k = self.quantize(sb_d)
+                sr_k.append(sb_k)
+                sb_counter += 1
+        k.append(tuple(sr_k))
+        return k
+
+    def dequantize_decom(k):
+        y = [self.dequantize(k[0])]
+        sb_counter = 1
+        for sr_k in k[1:]:
+            sr_y = []
+            for sb_k in sr_k: # sb = subband
+                sb_y = self.dequantize(sb_k)
+                sr_y.append(sb_y)
+                sb_counter += 1
+        y.append(tuple(sr_y))
+        return y
+
+    def read_decom(self):
+        LL = io.imread(f"{prefix}LL{self.levels}")
+        decom = [LL]
+        resolution_index = self.levels
+        for l in range(self.levels, 0, -1):
+            subband_names = ["LH", "HL", "HH"]
+            sb = 0
+            resolution = []
+            for sbn in subband_names:
+                resolution.appen(io.imread(f"{self.args.input}{sbn}{resolution_index}"))
+                sb += 1
+            decom.append(tuple(resolution))
+            resolution_index -= 1
+        return decom
+
+    def save_decom(self, decom):
+        LL = decom[0]
+        fn = f"{self.args.output}LL{self.levels}"
+        io.imsave(fn, LL)
+        self.required_bytes = os.path.getsize(fn)
+        resolution_index = self.levels
+        aux_decom = [decom[0][..., 0]]
+        for resolution in decom[1:]:
+            subband_names = ["LH", "HL", "HH"]
+            sb = 0
+            aux_resol = []
+            for sbn in subband_names:
+                fn = f"{self.args.output}{sbn}{resolution_index}"
+                io.imsage(fn, resolution[sb])
+                self.required_bytes = os.path.getsize(fn)
+                aux_resol.append(resolution[sb][..., 0])
+                sb += 1
+            resolution_index -= 1
+            aux_decom.append(tuple(aux_resol))
+        self.slices = pywt.coeffs_to_array(aux_decom)[1]
+        return slices
 
 if __name__ == "__main__":
     logging.info(__doc__)
