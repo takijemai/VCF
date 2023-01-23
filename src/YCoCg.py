6552396519e27344bb306e8cb2e1256929194879
JEMAI TAKIEDDINE <jt800@inlumine.ual.es>
2023-01-24 00:50:42 +0100

    ycocg

--- src/YCoCg.py
+++ src/YCoCg.py
@@ -10,35 +10,42 @@ import PNG as EC
 import deadzone as DZ
 import LloydMax as LM
 
+
 # pip install "color_transforms @ git+https://github.com/vicente-gonzalez-ruiz/color_transforms"
 from color_transforms.YCoCg import from_RGB
 from color_transforms.YCoCg import to_RGB
+# new command-line argument to specify the quantization method
+parser = argparse.ArgumentParser(
+    description="Exploiting color (perceptual) redundancy with the YCoCg transform.")
+parser.add_argument("--quantization", type=str, choices=[
+                    "deadzone", "lloydmax"], help="Choose the quantization method: deadzone or lloydmax")
 
 
-class CoDec(object):
-    def __init__(self, method):
-        self.method = method
-        if method == "deadzone":
-            self.quantize = DZ.quantize
-            self.dequantize = DZ.dequantize
-        elif method == "LloydMax":
-            self.quantize = LM.quantize
-            self.dequantize = LM.dequantize
-        else:
-            raise ValueError("Invalid quantization method")
+class CoDec(DZ.CoDec):
+    def __init__(self, args):
+        self.quantization = args.quantization
+        super().__init__(args)
 
     def encode(self):
         img = self.read()
         img_128 = img.astype(np.int16) - 128
         YCoCg_img = from_RGB(img_128)
-        k = self.quantize(YCoCg_img)
+        if self.quantization == 'deadzone':
+            k = self.quantize(YCoCg_img)
+        elif self.quantization == 'Lloydmax':
+            self.quantize = LM.quantize
+            k = LM.quantize(YCoCg_img)
         self.write(k)
         rate = (self.output_bytes*8)/(img.shape[0]*img.shape[1])
         return rate
 
     def decode(self):
         k = self.read()
-        YCoCg_y = self.dequantize(k)
+        if self.quantization == "deadzone":
+            YCoCg_y = self.dequantize(k)
+        elif self.quantization == "lloydmax":
+            self.dequantize = LM.dequantize
+            YCoCg_y = LM.dequantize(k)
         #y_128 = to_RGB(YCoCg_y.astype(np.int16))
         y_128 = to_RGB(YCoCg_y)
         y = (y_128.astype(np.int16) + 128)
@@ -49,8 +56,5 @@ class CoDec(object):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--method", choices=["deadzone", "LloydMax"],
-                        required=True, help="Quantization method to use")
     args = parser.parse_args()
-    main.main(EC.parser, logging, CoDec, args.method)
+    main.main(EC.parser, logging, CoDec)
