47da73f7c04105e7945fb1ff2384c75a15e6dfd3
JEMAI TAKIEDDINE <jt800@inlumine.ual.es>
2023-01-25 19:07:47 +0100

    Ipp up done

--- src/IPP.py
+++ src/IPP.py
@@ -8,7 +8,11 @@ images = []
 for i in range(1, 11):
     img = cv2.imread(
         r'C:\Users\Usuario\OneDrive\Bureau\Project VCF\env/image{}.jpg'.format(i))
-    images.append(img)
+    if img.shape[0] % 8 == 0 and img.shape[1] % 8 == 0:
+        images.append(img)
+    else:
+        print("Skipping image with shape {}".format(img.shape))
+
 
 # oyther methode:Input image sequence
 #images = [np.random.rand(256, 256) for i in range(10)]
@@ -20,16 +24,21 @@ quant_table = np.random.rand(8, 8)
 huffman_dict = {i: i for i in range(256)}
 encoded_data = []
 for img in images:
-    # Divide image into 8x8 blocks
-    blocks = np.array([img[i:i+8, j:j+8] for i in range(0, img.shape[0], 8)
-                      for j in range(0, img.shape[1], 8)])
+    if img.shape[0] % 8 == 0 and img.shape[1] % 8 == 0:
+        for channel in range(3):
+            # Divide image into 8x8 blocks
+            blocks = np.array([img[i:i+8, j:j+8, channel]
+                              for i in range(0, img.shape[0], 8) for j in range(0, img.shape[1], 8)])
 
     # Perform DCT on each block
     dct_blocks = np.array([dct(block, norm='ortho') for block in blocks])
 
     # Quantize the DCT coefficients
     quant_blocks = np.round(dct_blocks / quant_table)
-
+    # Make sure the coefficients are non-negative before looking them up in the Huffman dictionary
+    quant_blocks = np.abs(quant_blocks)
+    # Make sure the coefficients are within the range of the Huffman dictionary
+    quant_blocks[quant_blocks > 255] = 255
     # Encode the quantized DCT coefficients using Huffman coding
     encoded_blocks = [huffman_dict[coeff]
                       for block in quant_blocks for coeff in block.flatten()]
@@ -41,6 +50,8 @@ decoded_images = []
 for i in range(0, len(encoded_data), 64*64):
     # Decode the data using the Huffman decoding algorithm
     decoded_blocks = [huffman_dict[coeff] for coeff in encoded_data[i:i+64*64]]
+    # reshape the decoded_blocks array
+    decoded_blocks = np.array(decoded_blocks).reshape(-1, 8, 8)
 
     # De-quantize the DCT coefficients
     dct_blocks = decoded_blocks * quant_table
@@ -53,3 +64,16 @@ for i in range(0, len(encoded_data), 64*64):
     img = np.vstack([np.hstack(blocks[i:i+64])
                     for i in range(0, len(blocks), 64)])
     decoded_images.append(img)
+
+    # Display original image
+    cv2.imshow("Original Image", images[0])
+
+    # Display decoded image
+    cv2.imshow("Decoded Image", decoded_images[0])
+
+    # Wait for user to press a key before closing the images
+    cv2.waitKey(0)
+
+    # Close all windows
+    cv2.destroyAllWindows()
+    cv2.imwrite("decoded_image.jpg", decoded_images[0])
